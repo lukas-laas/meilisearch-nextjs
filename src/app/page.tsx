de3dc5497e1f5ac8771544f874becaed856a1a0b
lukas-laas <lukas.s.laas@gmail.com>
2024-10-01 16:58:39 +0200

    display results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { MeiliSearch } from "meilisearch";
 import movies from "./movies.json";
+import { useState } from "react";
 
 export default function Home() {
+  const [results, setResults] = useState<any>(null);
+
   const client = new MeiliSearch({
     host: "http://localhost:7700",
   });
@@ -13,10 +16,31 @@ export default function Home() {
 
   // console.log(client.getTask(0));
 
-  client
-    .index("movies")
-    .search("botman")
-    .then((res) => console.log(res));
+  // client
+  //   .index("movies")
+  //   .search("botman")
+  //   .then((res) => console.log(res));
 
-  return <div></div>;
+  // console.log("results:", results.hits[0].title);
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="text-black"
+        onChange={(q) =>
+          client
+            .index("movies")
+            .search(q.target.value)
+            .then((res) => setResults(res))
+        }
+      />
+      <ul>
+        {results &&
+          results.hits.map((result) => {
+            return <li key={result.id}>{result.title}</li>;
+          })}
+      </ul>
+    </div>
+  );
 }
