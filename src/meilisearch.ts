1ebf93590dd6e723fc78a86ecda9aaa106b09df6
lukas-laas <lukas.s.laas@gmail.com>
2024-10-02 15:07:57 +0200

    add functions for clearing and rebuilding developer search database

---
+++ src/meilisearch.ts
@@ -0,0 +1,26 @@
+import "server-only";
+
+import { MeiliSearch } from "meilisearch";
+import { getDevelopersQuery } from "./queries";
+const client = new MeiliSearch({
+  host: "http://localhost:7700",
+});
+
+export async function clearSearchDb() {
+  client.deleteIndexIfExists("developers");
+}
+
+async function buildSearchDb() {
+  const developers = await getDevelopersQuery();
+
+  client
+    .index("developers")
+    .addDocuments(developers)
+    .then((res) => console.log(res));
+}
+
+export async function rebuildSearchDb() {
+  await clearSearchDb();
+  await buildSearchDb();
+  return console.log("rebuilt search database");
+}
